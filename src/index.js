34b1ddbf30f392e2071cb1170ddb512e789f2693
Baraa-Nazar <baraa.nazar.ph@gmail.com>
2022-10-22 11:16:36 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+let form = document.querySelector("form")
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  bulidTodo(e.target.description.value)
+  form.reset()
+})
+}); 
+function bulidTodo(todo){
+  let p = document.createElement("p")
+  let btn = document.createElement("button")
+  btn.addEventListener("click", handelDelete)
+  btn.textContent ="delete"
+  btn.style.color = "red"
+  btn.style.fontSize = "10px"
+  p.style.fontSize = "20px"
+  p.textContent = `${todo} `
+  p.appendChild(btn)
+  document.querySelector("#tasks").appendChild(p)
+}
+function handelDelete(e){
+  e.target.parentNode.remove()
+}
+
